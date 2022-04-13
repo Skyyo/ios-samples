642dac76f5f886978f543bd4ca8cfedbfd7f62e5
skyyo <swifttropicbear@gmail.com>
2022-04-13 14:53:50 +0300

    Initial Commit

---
+++ ios-samples/ios_samplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_samplesApp.swift
+//  ios-samples
+//
+//  Created by Denis Rudenko on 13.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_samplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
